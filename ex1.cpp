79b200e9dc15e2634282fd87cd124d91a525ecc3
JS2108 <jj210895@gmail.com>
2020-06-22 11:07:38 -0500

    Ejercicio 1

--- ex1.cpp
+++ ex1.cpp
@@ -5,6 +5,7 @@ using namespace std;
 #include <string>
 
 int n=10;//Tamaño de la lista
+int arr_ctr=0;//contador de elementos de la lista
 
 int swaper(int *ptr1, int *ptr2){//Funcion swap usando punteros
   int temp;
@@ -14,18 +15,16 @@ int swaper(int *ptr1, int *ptr2){//Funcion swap usando punteros
     *ptr2=temp;}
   return 0;}
 
-int printer(int *ptr_p){//Imprime desde el puntero &ints[0]
+/*int printer(int *ptr_p){//Imprime desde el puntero &ints[0]
   for(int i=0;i<n;i++){
     cout<<*(ptr_p+i)<<" ";}
   cout<<endl;
+  return 0;}*/
+int printer(int *ptr_p){//Imprime desde el puntero &ints[0]
+  for(int i=0;i<arr_ctr;i++){
+    cout<<*(ptr_p+n-arr_ctr+i)<<" ";}
+  cout<<endl;
   return 0;}
-
-int finder(int *ptr_f, int x){//Encuentra el numero de elementos x
-  int ind;
-  for(int i=0;i<n;i++){
-    if(*(ptr_f+i)==x){
-      ind=i;break;}}
-  return ind;}
 
 int counter(int *ptr_ctr,int x){//Cuenta el numero de elementos x
   int count;
@@ -39,9 +38,18 @@ int sorter(int *ptr_a){ //Bubble sort con puntero &ints[0]
       if(*(ptr_a+j+1)<*(ptr_a+j))swaper(ptr_a+j,ptr_a+j+1);}}
   return 0;}
 
+  int earser(int *ptr_e, int a, int b){//Encuentra a y lo borra b veces
+    for(int j=0;j<b;j++){
+      for(int i=0;i<n;i++){
+        if(*(ptr_e+i)==a){
+          *(ptr_e+i)=0;
+          arr_ctr--;
+          sorter(ptr_e);break;}}}
+    return 0;}
+
 int main(){
   int ints[n];//array para la lista de enteros
-  int arr_ctr=0;//contador de elementos de la lista
+//  int arr_ctr=0;//contador de elementos de la lista
   int input,option;
   int *ptr;//puntero principal
   ptr=&ints[0];
@@ -56,17 +64,14 @@ int main(){
     cin>>option;
     switch(option){
       case 1: cin>>input;//si hay menos de n elementos, agrega la entrada en ints[0]
-              if(arr_ctr<n){ints[0]=input;arr_ctr++;sorter(ptr);break;}
+              if(arr_ctr<n){ints[0]=input;arr_ctr++;printer(ptr);sorter(ptr);printer(ptr);break;}
               else {cout<<"\nLista llena, presione Enter para continuar\n";
                 system("read"); break;}
       case 2: cout<<"\nSu lista es: "; printer(ptr);//muestra la lista
               cout<<"\nPresione Enter para continuar\n"; system("read"); break;
       case 3: cin>>input; //elimina el primer numero de la lista que indica la entrada
-              ints[finder(ptr,input)]=0;sorter(ptr);arr_ctr--;break;
-      case 4: cin>>input; for(int i=0;i<counter(ptr,input);i++){
-                            for(int j=0;j<n;j++){ints[finder(ptr,input)]=0;}
-                            arr_ctr--;}
-                          sorter(ptr);break;//elimina todos los numeros segun la entrada
+              earser(ptr,input,1);sorter(ptr);break;
+      case 4: cin>>input; earser(ptr,input,n); sorter(ptr);break;//elimina todo numero indicado
       default: if(option!=5)cout<<"\nOpcion no valida\n\n";}}
   while(option!=5);
 }
