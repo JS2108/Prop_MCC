e98b49f3010a80cffa714bfc56fc4c98705ce805
JS2108 <jj210895@gmail.com>
2020-06-21 20:57:15 -0500

    Ejercicio 1

--- ex1.cpp
+++ ex1.cpp
@@ -1,4 +1,4 @@
-//Ejercicio 1
+//ejercicio 1
 
 #include <iostream>
 using namespace std;
@@ -37,35 +37,36 @@ int sorter(int *ptr_a){ //Bubble sort con puntero &ints[0]
   for(int i=n-2;i>=0;i--){
     for(int j=i;j<=n-2;j++){
       if(*(ptr_a+j+1)<*(ptr_a+j))swaper(ptr_a+j,ptr_a+j+1);}}
-  printer(ptr_a);
   return 0;}
 
 int main(){
-  int ints[n];
-  int arr_ctr=0;
+  int ints[n];//array para la lista de enteros
+  int arr_ctr=0;//contador de elementos de la lista
   int input,option;
-  int *ptr;
+  int *ptr;//puntero principal
   ptr=&ints[0];
-  string text="Ingrese una acción: \n"
+  string text="\nIngrese una acción: \n"
                   "1. Agregar un entero\n"
                   "2. Mostrar lista de enteros\n"
                   "3. Eliminar el primer entero con un valor dado\n"
                   "4. Eliminar todos los enteros con un valor dado\n"
-                  "5. Salir\n";
+                  "5. Salir\n\n";
   do{
-    cout<<text;//Menu
-    cin>>option;//opcion
+    cout<<text;//imprime menu
+    cin>>option;
     switch(option){
-      case 1: cin>>input;
-              if(arr_ctr<n){ints[0]=input;printer(ptr);arr_ctr++;sorter(ptr);break;}
+      case 1: cin>>input;//si hay menos de n elementos, agrega la entrada en ints[0]
+              if(arr_ctr<n){ints[0]=input;arr_ctr++;sorter(ptr);break;}
               else {cout<<"\nLista llena, presione Enter para continuar\n";
                 system("read"); break;}
-      case 2: cout<<"\nSu lista es: "; printer(ptr);
+      case 2: cout<<"\nSu lista es: "; printer(ptr);//muestra la lista
               cout<<"\nPresione Enter para continuar\n"; system("read"); break;
-      case 3: cin>>input; ints[finder(ptr,input)]=0;sorter(ptr);arr_ctr--;break;
+      case 3: cin>>input; //elimina el primer numero de la lista que indica la entrada
+              ints[finder(ptr,input)]=0;sorter(ptr);arr_ctr--;break;
       case 4: cin>>input; for(int i=0;i<counter(ptr,input);i++){
                             for(int j=0;j<n;j++){ints[finder(ptr,input)]=0;}
                             arr_ctr--;}
-                          sorter(ptr);break;}
-      other: if(option!=5) cout<<"\nOpción no válida\n\n";}
-  while(option!=5);}
+                          sorter(ptr);break;//elimina todos los numeros segun la entrada
+      default: if(option!=5)cout<<"\nOpcion no valida\n\n";}}
+  while(option!=5);
+}
