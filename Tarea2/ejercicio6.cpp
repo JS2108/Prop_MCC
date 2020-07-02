2cbd3ad7b0121b6fc11a0ba8a135fa2bc747b93a
JS2108 <jj210895@gmail.com>
2020-07-02 11:02:16 -0500

    Ejercicio 6

---
+++ Tarea2/ejercicio6.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+bool f(int t[], int n, int &v){
+  int *ptr;
+  ptr=&v;
+  bool b;
+  bool flag;
+  for(int j=0;j<n && b!=true;j++){
+    if(t[j]>=0&&t[j]<=10){
+      *ptr=t[j];
+      b=true;}
+    else b=false;}
+  return b;
+}
+
+int main(){
+  int n,v;
+  cout<<"Ingrese el tamaño del vector: ";
+  cin>>n;
+  int arr[n];
+  cout<<"Ingrese el vector de enteros: ";
+  for(int i=0;i<n;i++)cin>>arr[i];
+  f(arr,n,v);
+    cout<<"El primer numero entre 1 y 10 encontrado es: "<<v<<endl;
+}
