526b783a2824beabaa78408927872e341f27c3bc
JS2108 <jj210895@gmail.com>
2020-07-02 12:56:49 -0500

    Ejercicio 10

---
+++ Tarea2/ejercicio10.cpp
@@ -0,0 +1,21 @@
+//ejercicio 10
+#include <iostream>
+using namespace std;
+
+int main(){
+  int n,*arr,*sq;
+  cout<<"Ingrese el tamaño del vector: ";
+  cin>>n;
+  arr=new int [n];
+  sq=new int [n];
+  cout<<"Ingrese el vector: ";
+  for(int i=0;i<n;i++)//Captura arr[]
+    cin>>arr[i];
+  for(int i=0;i<n;i++)//Llena sq[] con los cuadrados de arr[]
+    sq[i]=arr[i]*arr[i];
+  delete[] arr;//Borra arr
+  for(int i=0;i<n;i++)//Imprime sq[]
+    cout<<sq[i]<<" ";
+  cout<<endl;
+  delete[] sq;//Borra sq
+}
