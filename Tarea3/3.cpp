dc21fc921e62bfcaf730d448b4c6584c2eade380
JS2108 <jj210895@gmail.com>
2020-07-19 18:47:18 -0500

    Ejercicio 3 output decimal

--- Tarea3/3.cpp
+++ Tarea3/3.cpp
@@ -4,28 +4,41 @@ using namespace std;
 #include "lqueue.h"
 #include <math.h>
 
-void rotacion_bits(int registro, int d,int lado){
-  int reg_size=sizeof(d)*8;//tamaño del registro
-  LQueue<bool> Q(reg_size);//lista ligada circular (cola cerrada)
+void int_to_bits(LQueue<bool> &Q,int registro, int reg_size){
   for(int i=0;i<=reg_size-1;i++){//llena la cola con los bits de registro
-  if(registro>=(unsigned int)pow(2,reg_size-1-i)){
-    Q.enqueue(true);
-    registro-=(unsigned int)pow(2,reg_size-1-i);}
-  else
-    Q.enqueue(false);}
-  Q.print();
-  for(int i=0;i<d;i++){//número de veces que se rota
-    if(lado==1) Q.right_roll();//si lado==1 rota a la derecha
+    if(registro>=(unsigned int)pow(2,reg_size-1-i)){
+      Q.enqueue(true);
+      registro-=(unsigned int)pow(2,reg_size-1-i);}
+    else
+      Q.enqueue(false);}
+  }
+
+void bits_to_int(LQueue<bool> &Q,int &registro, int reg_size){
+  bool temp;
+  unsigned int t_reg;
+  for(int i=reg_size-1;i>=0;i--){//llena la cola con los bits de registro
+    Q.dequeue(temp);
+    if(temp==true)
+      t_reg+=pow(2,i);}
+  registro=int(t_reg);
+}
+
+void rotacion_bits(int &registro, int d,int lado){
+  cout<<"Registro decimal inicial: "<<registro<<endl;
+  int reg_size=sizeof(d)*8;
+  LQueue<bool> Q(reg_size);
+  int_to_bits(Q,registro,reg_size);
+  for(int i=0;i<d;i++){             //número de veces que se rota
+    if(lado==1) Q.right_roll();     //si lado==1 rota a la derecha
     else{
-      if(lado==0) Q.left_roll();}}//si lado==0 rota a la izquierda
-  Q.print();
+      if(lado==0) Q.left_roll();}}  //si lado==0 rota a la izquierda
+  bits_to_int(Q,registro,reg_size);
+  cout<<"Registro decimal rotado: "<<registro<<endl;
 }
 
 int main(){
-  int a=0x88A87888;
-  rotacion_bits(a,5,1);//rota "a" 5 veces a la derecha
-  cout<<endl;
-  rotacion_bits(a,5,0);//rota "a" 5 veces a la izquerda
+  int a=0x8;
+  rotacion_bits(a,2,0);
 }
 
 /*
